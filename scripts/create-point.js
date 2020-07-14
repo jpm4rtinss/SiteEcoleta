433a3c54e8ad1ad842784c3a50b98507171265f6
João Pedro Martins <joaopedromsn@gmail.com>
2020-07-13 21:17:35 -0300

    Corrigindo bug e enviando valores para back-end

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -25,25 +25,26 @@ function getCities(event){
     stateInput.value = event.target.options[indexOfSelectedState].text;
     
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-    citySelect.innerHTML = ""
-    citySelect.disabled = true
+
+        citySelect.innerHTML = "<option value>Selecione a Cidade</option"
+        citySelect.disabled = true
 
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
-        citySelect.innerHTML = "<option value>Selecione a Cidade</option"
+        
         for(const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>` 
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>` 
         }
         citySelect.disabled = false
     }  )
 }
 
 
-
 document
       .querySelector("select[name=uf]")
       .addEventListener("change", getCities)
+
 
 const itemsToCollect = document.querySelectorAll(".items-grid li")
 
@@ -51,21 +52,22 @@ for (const item of itemsToCollect){
     item.addEventListener("click", handleSelectedItem)
 }
 
+const collectedItems = document.querySelector('input[name=items]')
+
 let selectedItems = []
 
 function handleSelectedItem(event) {
     const itemLi = event.target
-
     itemLi.classList.toggle("selected")
 
     const itemId = itemLi.dataset.id
 
  
     const alreadySelected = selectedItems.findIndex(item => {
-        const itemFound = item == itemLi
+        const itemFound = item == itemId
         return itemFound
     })
-
+    
     if ( alreadySelected >= 0) {
         const filteredItems = selectedItems.filter ( item =>{
             const itemIsDifferent= item != itemId 
@@ -76,5 +78,5 @@ function handleSelectedItem(event) {
     } else{
         selectedItems.push(itemId)
     }
-
+    collectedItems.value = selectedItems
 }
